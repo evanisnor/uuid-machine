fd89801fd0a4a68cd087da9ee15b0b155b4190d8
leadhead9 <ewisnor@gmail.com>
2013-12-26 22:39:00 -0400

    Renaming app directory to src

---
+++ src/enum/uuidformat.js
@@ -0,0 +1,18 @@
+/*
+ * uuidformat.js
+ */
+define([],
+	function() {
+		return {
+			Uppercase : {
+				label: 'uppercase'
+			},
+			Hyphens : {
+				label: 'hyphens'
+			},
+			Braces : {
+				label: 'braces'
+			}
+		};
+	}
+);
